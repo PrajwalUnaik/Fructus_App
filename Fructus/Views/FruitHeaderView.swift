5dd5ab9be422ca808bf5f505056ccb31fe158c3d
prajwalnaik28@gmail.com <prajwalnaik28@gmail.com>
2023-12-26 22:34:52 +0530

    New sheet

---
+++ Fructus/Views/FruitHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitHeaderView.swift
+//  Fructus
+//
+//  Created by Prajwal U on 26/12/23.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitHeaderView()
+}
---
+++ Fructus/Views/fruitDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  fruitDetailView.swift
+//  Fructus
+//
+//  Created by Prajwal U on 26/12/23.
+//
+
+import SwiftUI
+
+struct fruitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    fruitDetailView()
+}
