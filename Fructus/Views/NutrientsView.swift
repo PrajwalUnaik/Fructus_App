18fa3520295e5b085b2533f31160015e6a8fbef0
prajwalnaik28@gmail.com <prajwalnaik28@gmail.com>
2023-12-27 11:18:54 +0530

    nutrients view

---
+++ Fructus/Views/NutrientsView.swift
@@ -0,0 +1,18 @@
+//
+//  NutrientsView.swift
+//  Fructus
+//
+//  Created by Prajwal U on 27/12/23.
+//
+
+import SwiftUI
+
+struct NutrientsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NutrientsView()
+}
